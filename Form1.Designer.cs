0c048d4d6bdd743d6ee6f67e045a9d8386a96088
jordhardwell <jordhardwell@gmail.com>
2020-01-29 10:35:59 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkSong = new System.Windows.Forms.Timer(this.components);
+            this.nowPlayingArtist = new System.Windows.Forms.Label();
+            this.searchSong = new System.Windows.Forms.Button();
+            this.header = new System.Windows.Forms.PictureBox();
+            this.nowPlayingSong = new System.Windows.Forms.Label();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.npLink = new System.Windows.Forms.Button();
+            this.clipboard = new System.Windows.Forms.Label();
+            this.clipUpdate = new System.Windows.Forms.Timer(this.components);
+            this.searchInput = new System.Windows.Forms.TextBox();
+            this.regexOut = new System.Windows.Forms.Label();
+            this.goToLink = new System.Windows.Forms.Button();
+            this.cleanLinkBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.header)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkSong
+            // 
+            this.checkSong.Enabled = true;
+            this.checkSong.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // nowPlayingArtist
+            // 
+            this.nowPlayingArtist.AutoSize = true;
+            this.nowPlayingArtist.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nowPlayingArtist.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.nowPlayingArtist.Location = new System.Drawing.Point(239, 77);
+            this.nowPlayingArtist.Name = "nowPlayingArtist";
+            this.nowPlayingArtist.Size = new System.Drawing.Size(86, 37);
+            this.nowPlayingArtist.TabIndex = 3;
+            this.nowPlayingArtist.Text = "label1";
+            // 
+            // searchSong
+            // 
+            this.searchSong.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.searchSong.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.searchSong.Location = new System.Drawing.Point(0, 273);
+            this.searchSong.Name = "searchSong";
+            this.searchSong.Size = new System.Drawing.Size(557, 23);
+            this.searchSong.TabIndex = 4;
+            this.searchSong.Text = "Search";
+            this.searchSong.UseVisualStyleBackColor = true;
+            this.searchSong.Click += new System.EventHandler(this.searchSong_Click);
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.Purple;
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(557, 19);
+            this.header.TabIndex = 5;
+            this.header.TabStop = false;
+            // 
+            // nowPlayingSong
+            // 
+            this.nowPlayingSong.AutoSize = true;
+            this.nowPlayingSong.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nowPlayingSong.ForeColor = System.Drawing.Color.Silver;
+            this.nowPlayingSong.Location = new System.Drawing.Point(239, 40);
+            this.nowPlayingSong.Name = "nowPlayingSong";
+            this.nowPlayingSong.Size = new System.Drawing.Size(86, 37);
+            this.nowPlayingSong.TabIndex = 0;
+            this.nowPlayingSong.Text = "label1";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Red;
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.Location = new System.Drawing.Point(540, 1);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(16, 16);
+            this.exitBtn.TabIndex = 6;
+            this.exitBtn.Text = "X";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // npLink
+            // 
+            this.npLink.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.npLink.Location = new System.Drawing.Point(0, 250);
+            this.npLink.Name = "npLink";
+            this.npLink.Size = new System.Drawing.Size(557, 23);
+            this.npLink.TabIndex = 7;
+            this.npLink.Text = "Now Playing Link";
+            this.npLink.UseVisualStyleBackColor = true;
+            this.npLink.Click += new System.EventHandler(this.npLink_Click);
+            // 
+            // clipboard
+            // 
+            this.clipboard.AutoSize = true;
+            this.clipboard.Location = new System.Drawing.Point(13, 26);
+            this.clipboard.Name = "clipboard";
+            this.clipboard.Size = new System.Drawing.Size(35, 13);
+            this.clipboard.TabIndex = 8;
+            this.clipboard.Text = "label1";
+            // 
+            // clipUpdate
+            // 
+            this.clipUpdate.Enabled = true;
+            this.clipUpdate.Interval = 1000;
+            this.clipUpdate.Tick += new System.EventHandler(this.clipUpdate_Tick);
+            // 
+            // searchInput
+            // 
+            this.searchInput.Location = new System.Drawing.Point(13, 103);
+            this.searchInput.Name = "searchInput";
+            this.searchInput.Size = new System.Drawing.Size(100, 20);
+            this.searchInput.TabIndex = 9;
+            this.searchInput.TextChanged += new System.EventHandler(this.searchInput_TextChanged);
+            // 
+            // regexOut
+            // 
+            this.regexOut.AutoSize = true;
+            this.regexOut.Location = new System.Drawing.Point(13, 40);
+            this.regexOut.Name = "regexOut";
+            this.regexOut.Size = new System.Drawing.Size(35, 13);
+            this.regexOut.TabIndex = 10;
+            this.regexOut.Text = "label1";
+            // 
+            // goToLink
+            // 
+            this.goToLink.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.goToLink.Location = new System.Drawing.Point(0, 227);
+            this.goToLink.Name = "goToLink";
+            this.goToLink.Size = new System.Drawing.Size(557, 23);
+            this.goToLink.TabIndex = 11;
+            this.goToLink.Text = "Follow clip link";
+            this.goToLink.UseVisualStyleBackColor = true;
+            this.goToLink.Click += new System.EventHandler(this.goToLink_Click);
+            // 
+            // cleanLinkBtn
+            // 
+            this.cleanLinkBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.cleanLinkBtn.Location = new System.Drawing.Point(0, 204);
+            this.cleanLinkBtn.Name = "cleanLinkBtn";
+            this.cleanLinkBtn.Size = new System.Drawing.Size(557, 23);
+            this.cleanLinkBtn.TabIndex = 12;
+            this.cleanLinkBtn.Text = "Clean Links";
+            this.cleanLinkBtn.UseVisualStyleBackColor = true;
+            this.cleanLinkBtn.Click += new System.EventHandler(this.cleanLinkBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.searchSong;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(90)))), ((int)(((byte)(119)))));
+            this.ClientSize = new System.Drawing.Size(557, 296);
+            this.Controls.Add(this.cleanLinkBtn);
+            this.Controls.Add(this.goToLink);
+            this.Controls.Add(this.regexOut);
+            this.Controls.Add(this.searchInput);
+            this.Controls.Add(this.clipboard);
+            this.Controls.Add(this.npLink);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.header);
+            this.Controls.Add(this.searchSong);
+            this.Controls.Add(this.nowPlayingArtist);
+            this.Controls.Add(this.nowPlayingSong);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.header)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer checkSong;
+        private System.Windows.Forms.Label nowPlayingArtist;
+        private System.Windows.Forms.Button searchSong;
+        private System.Windows.Forms.PictureBox header;
+        private System.Windows.Forms.Label nowPlayingSong;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button npLink;
+        private System.Windows.Forms.Label clipboard;
+        private System.Windows.Forms.Timer clipUpdate;
+        private System.Windows.Forms.TextBox searchInput;
+        private System.Windows.Forms.Label regexOut;
+        private System.Windows.Forms.Button goToLink;
+        private System.Windows.Forms.Button cleanLinkBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string currentSong, npSong, npArtist, searchTerm;
+        private Process[] processList;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // nowPlayingSong.Text = currentSong;
+        }
+
+        private bool openSpotify(string uri)
+        {
+            try
+            {
+                using (Process spotify = new Process())
+                {
+                    spotify.StartInfo.FileName = uri;
+                    spotify.Start();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Whoops");
+                return false;
+            }
+        } // Open spotify URIs
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                //convertLink(Clipboard.GetText());
+
+                //System.Windows.Forms.Application.Exit();
+        }//Exit Button
+
+        private void npLink_Click(object sender, EventArgs e)
+        {
+            string toClip = "spotify:search:" + nowPlayingArtist.Text + nowPlayingSong.Text;
+            Clipboard.SetText(toClip.Replace(" ", "+"));
+        }
+
+        private string convertLink(string link) {
+            // Sample input https://open.spotify.com/track/5SiZJoLXp3WOl3J4C8IK0d?si=VWoGtyBGS4mgM6A9rN7fAQ
+            string input = link; 
+            String regexTrack = "(?:/track)\\S*";
+            String regexPlaylist = "(?:/playlist)\\S*";
+            String regexArtist = "(?:/artist)\\S*";
+            String regexAlbum = "(?:/album)\\S*";
+            String regexUser = "(?:/user)\\S*";
+
+            if (Regex.Match(input, regexTrack).Success) {
+                input = Regex.Match(input, regexTrack).ToString();
+            }else if (Regex.Match(input, regexPlaylist).Success)
+            {
+                input = Regex.Match(input, regexPlaylist).ToString();
+            }else if (Regex.Match(input, regexArtist).Success)
+            {
+                input = Regex.Match(input, regexArtist).ToString();
+            }else if (Regex.Match(input, regexAlbum).Success)
+            {
+                input = Regex.Match(input, regexAlbum).ToString();
+            }else if (Regex.Match(input, regexUser).Success)
+            {
+                input = Regex.Match(input, regexUser).ToString();
+            }
+            if (input.Contains("?"))
+            {
+                char question = '?';
+                string[] output = input.Split(question);
+                input = output[0];
+            }
+
+            //Clipboard.SetText(input);
+            input = input.Replace('/', ':');
+            if (!input.Contains("spotify")) {
+                input = "spotify" + input;
+            }
+            
+            Clipboard.SetText(input);
+            return input;
+
+            //Regex removeURL = new Regex(@"\.*$\");
+            //Regex removePromo = new Regex(@"?.*$\");
+
+            //string text = input.Replace(input, "(?:/track)S*");
+        }
+
+        private void searchInput_TextChanged(object sender, EventArgs e)
+        {
+            searchTerm = searchInput.Text;
+            Clipboard.SetText(searchTerm);
+        } // Updates Clipboard based on user input
+
+        private void goToLink_Click(object sender, EventArgs e)
+        {
+            string str = Clipboard.GetText();
+            if (str.Contains("spotify:"))
+            {
+                openSpotify(str);  
+            }
+        }
+
+        private void cleanLinkBtn_Click(object sender, EventArgs e)
+        {
+            convertLink(Clipboard.GetText());
+        }
+
+        private bool detectSpotifyLink(string data)
+        {
+            if (data.Contains("spotify"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } // Detect if Clipboard contains 'spotify'
+
+        private void searchSong_Click(object sender, EventArgs e) // Search for either now playing or input term
+        {
+            string preUri = $"spotify:search:{searchTerm}";
+            openSpotify(preUri);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {            
+            processList = Process.GetProcessesByName("Spotify");
+            foreach (Process process in processList)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle) && process.ProcessName == "Spotify")
+                {
+                    currentSong = process.MainWindowTitle;
+                    if (currentSong != nowPlayingSong.Text) {
+                        string[] details = currentSong.Split('-');
+                        if (details.Length > 1) {
+                            nowPlayingSong.Text = details[1];
+                            nowPlayingArtist.Text = details[0];
+                        }                          
+                    }                    
+                }
+            }
+        } // Updates now playing from Spotify
+
+        private void clipUpdate_Tick(object sender, EventArgs e) //Timer to detect clipboard updates.
+        {
+            string clipData = Clipboard.GetText(); // Stores clip data
+            if (detectSpotifyLink(clipData) == true)
+            {
+                clipboard.Text = clipData;
+                if (clipboard.Text.Length > 90) {
+                    //Spotify Link is 90 chars long, most likely a batch.
+                    string[] links = clipboard.Text.Split(
+                        new[] { Environment.NewLine },
+                        StringSplitOptions.None
+                    );
+                    foreach (string link in links) {
+
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
